deedff90a9d1d6b6cfcba3b54125db7bdbb5f9ab
Sebastián Ávila A <sebastian4j@gmail.com>
2019-08-18 23:23:40 -0400

    versión inicial

---
+++ src/main/java/com/sebastian/redis/streams/StreamCliente.java
@@ -0,0 +1,49 @@
+package com.sebastian.redis.streams;
+
+import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.XReadArgs;
+import io.lettuce.core.XReadArgs.StreamOffset;
+
+/**
+ * consume los mensajes del stream.
+ * 
+ * info:
+ * 
+ * https://github.com/lettuce-io/lettuce-core/releases
+ * 
+ * @author Sebastián Ávila A.
+ *
+ */
+public class StreamCliente {
+  private static final Logger LOGGER = LoggerFactory.getLogger(StreamCliente.class);
+
+  /**
+   * el objetivo es leer todos mensajes disponibles.
+   * 
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) {
+    final var cliente = RedisClient.create("redis://localhost:6379");
+    String ultimo = "0";
+    boolean leyendo = true;
+    var cuentaMensajes = 0;
+    try (final var con = cliente.connect()) {
+      final var cmd = con.sync();
+      while (leyendo) {
+        final var mensajes = cmd.xread(XReadArgs.Builder.block(Duration.ofSeconds(5)).count(1),
+            StreamOffset.from("mensajes", ultimo));
+        cuentaMensajes += mensajes.size();
+        LOGGER.info("leidos: {}", cuentaMensajes);
+        for (final var m : mensajes) {
+          LOGGER.info("{}", m.getId());
+          ultimo = m.getId();
+        }
+      }
+    }
+    cliente.shutdown();
+  }
+}
---
+++ src/main/java/com/sebastian/redis/streams/StreamProductor.java
@@ -0,0 +1,38 @@
+package com.sebastian.redis.streams;
+
+import java.util.Map;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.lettuce.core.RedisClient;
+
+/**
+ * productor de mensajes en el stream.
+ * 
+ * https://github.com/lettuce-io/lettuce-core/wiki/Connection-Pooling
+ * 
+ * https://github.com/lettuce-io/lettuce-core/releases
+ * 
+ * @author Sebastián Ávila A.
+ *
+ */
+public class StreamProductor {
+  private static final Logger LOGGER = LoggerFactory.getLogger(StreamProductor.class);
+  private static final int MENSAJES_ENVIAR = 500_00;
+
+  public static void main(String[] args) throws InterruptedException {
+    final var cliente = RedisClient.create("redis://localhost:6379");
+    try (final var con = cliente.connect()) {
+      final var cmd = con.sync();
+      int i = 0;
+      while (i < MENSAJES_ENVIAR) {
+        final var res =
+            cmd.xadd("mensajes", Map.of(UUID.randomUUID().toString(), String.valueOf(i)));
+        i++;
+        LOGGER.info("enviados: {} - id: {}", i, res);
+        Thread.sleep(1_000);
+      }
+    }
+    cliente.shutdown();
+  }
+}
